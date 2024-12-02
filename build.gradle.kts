ed9dbc1bc54c799a9f66c5769656dd429c5544af
theaffable <kuba.szecowka@gmail.com>
2024-12-02 21:44:03 +0100

    Added required dependencies using toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(platform(libs.spring.boot.bom))
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.spring.boot.starter.web)
     testImplementation(kotlin("test"))
+    testImplementation(libs.bundles.test)
 }
 
 tasks.test {
@@ -18,4 +24,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+    compilerOptions {
+        // enable kotlin support for JSR 305 annotations (@NotNull etc.) - this makes dealing with Spring nullable types less painful
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }

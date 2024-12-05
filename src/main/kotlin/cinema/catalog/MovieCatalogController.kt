8d3fcee0f8f9f92f661c7a5bad19be69c445db1f
theaffable <kuba.szecowka@gmail.com>
2024-12-05 18:41:07 +0100

    Added update rating stub

--- src/main/kotlin/cinema/catalog/MovieCatalogController.kt
+++ src/main/kotlin/cinema/catalog/MovieCatalogController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -21,13 +22,19 @@ class MovieCatalogController(private val movieCatalogService: MovieCatalogServic
 
     @PostMapping("/{movie_catalog_id}/rating")
     @ResponseStatus(HttpStatus.CREATED)
-    fun rate(@PathVariable("movie_catalog_id") movieCatalogId: String, @RequestBody request: RatingRequest): RatingResponse {
+    fun createRating(@PathVariable("movie_catalog_id") movieCatalogId: String, @RequestBody request: RatingRequest): RatingResponse {
         if (!request.isValid()) throw RatingValueOutOfRangeException(request.rating, min = 1, max = 5)
         val newRating = movieCatalogService.rate(movieCatalogId.asMovieCatalogId(), request.rating)
         return RatingResponse(
             rating = request.rating,
             newMovieRating = newRating
         )
+    }
+
+    @PutMapping("/{movie_catalog_id}/rating")
+    fun updateRating(@PathVariable("movie_catalog_id") movieCatalogId: String, @RequestBody request: RatingRequest): RatingResponse {
+        if (!request.isValid()) throw RatingValueOutOfRangeException(request.rating, min = 1, max = 5)
+        throw NotImplementedError() // this can't be implemented with current repository implementation but will be possible once I add a proper database
     }
 }
 
